85ddf0506ad5faec479c74b8124f4596d20de4c2
PC-航\Administrator <651196684@qq.com>
2016-09-21 13:01:32 +0800

    Finish Homework 2

---
+++ jinzita/jzt.js
@@ -0,0 +1,16 @@
+// TypeScript file
+var x = process.argv[2];
+var num = parseInt(x);
+var a, b, c;
+var star, space;
+for (a = 0; a < num; a++) {
+    star = '';
+    space = '';
+    for (b = 0; b < (num - a - 1); b++) {
+        space = space + ' ';
+    }
+    for (c = 0; c < (2 * a + 1); c++) {
+        star = star + '*';
+    }
+    console.log(space + star);
+}
---
+++ jinzita/jzt.ts
@@ -0,0 +1,23 @@
+// TypeScript file
+
+
+
+var x = process.argv[2];
+var num:number = parseInt(x);
+
+var a,b,c:number;
+var star,space:string;
+
+for(a=0;a<num;a++){
+    star = '';
+    space = '';
+    for(b=0;b<(num-a-1);b++){
+        space = space +' ';
+
+    }
+    
+    for(c=0;c<(2*a+1);c++){
+        star = star + '*';
+    }
+      console.log(space + star);
+}
